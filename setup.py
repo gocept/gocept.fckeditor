625eafc4308a3027ebac763f4e10bcb11e98a3fd
Christian Zagrodnick <cz@gocept.com>
2007-05-14 13:26:10 +0000

    import fckeditor, not using svn:external to be able to make changes

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+name = "gocept.fckeditor"
+setup(
+    name = name,
+    version = "2.4",
+    author = "Christian Zagrodnick",
+    author_email = "cz@gocept.com",
+    description = "FCKEditor integration into Zope 3",
+    long_description = "XXX",
+    license = "ZPL 2.1",
+    url='https://svn.gocept.com/repos/gocept/gocept.fckeditor',
+
+    packages = find_packages('src'),
+    include_package_data = True,
+    package_dir = {'':'src'},
+    namespace_packages = ['gocept'],
+    install_requires = [
+        'setuptools',
+        'zc.resourcelibrary'
+    ]
+    )
---
+++ src/gocept/fckeditor/fckeditor/_test/manual/_common/manual_test.js
@@ -0,0 +1,3 @@
+﻿if ( typeof FCKScriptLoader != 'undefined' )
+	FCKScriptLoader.FCKeditorPath = document.location.pathname.substring(0,document.location.pathname.lastIndexOf('_test')) ;
+
